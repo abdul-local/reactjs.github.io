5f89fedfe9536169dd04c88031ee7f195a3177d7
abdul-local <abdullah.hamzan@gmail.com>
2020-10-17 17:52:22 +0800

    deploy

---
+++ precache-manifest.3534fbe5d2619c96ab45ccae85b1edf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6eb7b1ece5c3450902",
+    "url": "/static/css/main.4ccdc75d.chunk.css"
+  },
+  {
+    "revision": "db6eb7b1ece5c3450902",
+    "url": "/static/js/main.db6eb7b1.chunk.js"
+  },
+  {
+    "revision": "acc3af5258a87d258b5a",
+    "url": "/static/js/1.acc3af52.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2da075ac49b567ac69cd8554a5890529",
+    "url": "/index.html"
+  }
+];
